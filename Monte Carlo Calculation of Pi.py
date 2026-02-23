f09caf159f88e69e412d2e0b5b1a7db1c809833d
kimyu-111 <kimyou3000@naver.com>
2026-02-23 23:56:35 +0900

    Monte Carlo Calculation of Pi

---
+++ Monte Carlo Calculation of Pi.py
@@ -0,0 +1,24 @@
+from vpython import *
+import math
+
+g1= graph(width=400,height=380,ymax=1.1,xmax=1.1)
+fq=gdots(color=color.red)
+fo=gdots(color=color.blue)
+
+N=10000
+n=0
+cin=0
+
+while n<N:
+  rate(5000)
+  r=vector(random(),random(),0)
+  if mag(r)<1:
+    cin=cin+1
+    fq.plot(r.x,r.y)
+  else:
+    fo.plot(r.x,r.y)
+  
+n=n+1
+
+tpi=cin*4/n
+print("pi = ",tpi)
